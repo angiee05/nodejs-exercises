64fcfc07d1bff38ce8d1d324a8e0cfe13357bc93
angiee05 <angela1d050302@gmail.com>
2022-07-31 13:17:35 +0200

    Log The Data

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config"
-
-import app from "./app";
-
-const port = process.env.PORT;
-
-app.listen(port, () => {
-  console.log(`[server]: Server Is Running At http:/localhost:${port}`);
-});
