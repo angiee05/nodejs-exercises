8373355a63061eb2dc9c130d873cbbf562929e3b
angiee05 <angela1d050302@gmail.com>
2022-08-02 01:44:55 +0200

    create db and prisma model

--- dist/app.js
+++ dist/app.js
@@ -5,9 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("express-async-errors");
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
-app.get("/cities", (request, response) => {
-    response.json([{ city: "Naples" }, { city: "Florence" }]);
+app.get("/cities", async (request, response) => {
+    const cities = await prisma.city.findMany();
+    response.json(cities);
 });
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import "express-async-errors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 
-app.get("/cities", (request, response) => {
-  response.json([{ city: "Naples" }, { city: "Florence" }]);
+app.get("/cities", async (request, response) => {
+  const cities = await prisma.city.findMany();
+
+  response.json(cities);
 });
 
 export default app;
